25e6e10f840f7f85ba24ffd7dccd97809b8f5153
Nathan Annareddy <nathan.annareddy@gmail.com>
2023-11-06 21:08:09 -0600

    added some test scripts

--- testscripts/file.ml
+++ testscripts/file.ml
@@ -1,11 +1,4 @@
 open Curses
 
-let _=
-	let win = Curses.initscr() in
-	
-	let _ =
-		if mvwaddstr win 0 0 "Hello, Curses!" then
-			Curses.refresh();
-			Curses.ignore(Curses.getch());
-			Curses.endwin
+let _ = print_endline "yay"
 
