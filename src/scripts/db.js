0e1138127b4b11a192e5eda1fa86e8afbf92fa99
Nathan Annareddy <nathan.annareddy@gmail.com>
2024-01-25 18:43:59 -0600

    add IndexedDB for player and floor data

--- src/scripts/db.js
+++ src/scripts/db.js
@@ -1,32 +1,106 @@
-const indexedDB =
-    window.indexedDB ||
-    window.mozIndexedDB ||
-    window.webkitIndexedDB ||
-    window.msIndexedDB ||
-    window.shimIndexedDB;
+function addData(playerData, floorData){
+	const indexedDB =
+	window.indexedDB ||
+	window.mozIndexedDB ||
+	window.webkitIndexedDB ||
+	window.msIndexedDB ||
+	window.shimIndexedDB;
 
-const request = indexedDB.open('storage', 1);
-request.onerror() = function (event) {
-    console.error('An error occurred opening IndexedDB');
-    console.log(event);
-};
-request.onupgradeneeded() = function () {
-    const db = request.result;
-    const store = db.createObjectStore("playerData", {keypath: "id", autoIncrement: true});
-    store.createIndex('player', 'player', {unique: false});
-    store.createIndex('floor', 'floor', {unique: false});
+	const request = indexedDB.open("ErugoData", 1);
+	
+	request.onerror = function (event) {
+	  console.error("An error occurred with IndexedDB");
+	  console.error(event);
+	};
 
-};
-request.onsuccess() = function () {
-    const db = request.result;
-    const transaction = db.transaction('storage', 'readwrite');
-    const store = transaction.objectStore('storage');
-    const playerIndex = store.index('player');
-    const floorIndex = store.index('floor');
+	request.onupgradeneeded = function () {
+	  const db = request.result;
+	  const store = db.createObjectStore("user", { keyPath: "id" });
+	  store.createIndex("player", "player", { unique: false });
+	  store.createIndex("floor", "floor", {
+		unique: false,
+	  });
+	};
 
-    store.put({id: 1, player: "player1", floor: "floor2"});
-    const idQuery = store.get(1);
-    const playerQuery = playerIndex.getAll('player1');
+	request.onsuccess = function () {
+		console.log("Database opened successfully");
+
+		const db = request.result;
+		const transaction = db.transaction("user", "readwrite");
+		const store = transaction.objectStore('user');
+		
+		store.put({id: 1, player: playerData, floor: floorData});
+	};
 }
 
+function getData(){
+	const indexedDB =
+	window.indexedDB ||
+	window.mozIndexedDB ||
+	window.webkitIndexedDB ||
+	window.msIndexedDB ||
+	window.shimIndexedDB;
 
+	const request = indexedDB.open("ErugoData", 1);
+	
+	request.onerror = function (event) {
+	  console.error("An error occurred with IndexedDB");
+	  console.error(event);
+	};
+
+	request.onupgradeneeded = function () {
+	  const db = request.result;
+	  const store = db.createObjectStore("user", { keyPath: "id" });
+	  store.createIndex("player", "player", { unique: false });
+	  store.createIndex("floor", "floor", {
+		unique: false,
+	  });
+	};
+
+	request.onsuccess = function () {
+		console.log("Database opened successfully");
+
+		const db = request.result;
+		const transaction = db.transaction("user", "readwrite");
+		const store = transaction.objectStore('user');
+		
+		const result = store.get(1);
+		result.onsuccess = function(){
+			console.log(result.result);
+		}
+	};
+}
+
+function deleteData(){
+	const indexedDB =
+	window.indexedDB ||
+	window.mozIndexedDB ||
+	window.webkitIndexedDB ||
+	window.msIndexedDB ||
+	window.shimIndexedDB;
+
+	const request = indexedDB.open("ErugoData", 1);
+	
+	request.onerror = function (event) {
+	  console.error("An error occurred with IndexedDB");
+	  console.error(event);
+	};
+
+	request.onupgradeneeded = function () {
+	  const db = request.result;
+	  const store = db.createObjectStore("user", { keyPath: "id" });
+	  store.createIndex("player", "player", { unique: false });
+	  store.createIndex("floor", "floor", {
+		unique: false,
+	  });
+	};
+
+	request.onsuccess = function () {
+		console.log("Database opened successfully");
+
+		const db = request.result;
+		const transaction = db.transaction("user", "readwrite");
+		const store = transaction.objectStore('user');
+		store.delete(1);
+	};
+}
