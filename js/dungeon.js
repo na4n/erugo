32ae8854779168baea1f84554e63930ad0831e79
Nathan Annareddy <nathan.annareddy@gmail.com>
2024-02-16 11:13:12 -0600

    new celebration

--- js/dungeon.js
+++ js/dungeon.js
@@ -474,7 +474,8 @@ function saveData(){
 }
 
 function displayGameOver(endCondition){
-	let finalText = endCondition > 0 ? '&#x1F389YOU WON&#x1F389' : 'GAME OVER'; 
+	const arr = ['&#x1F389', '&#x1F973', '&#x1F483', '&#x1F57A', '&#x1F37E', '&#x1F37B', '&#129321', '&#127880', '&#x1F9A9']
+	let finalText = endCondition > 0 ? `${arr[Math.floor(Math.random()*(arr.length-1))]}YOU WON${arr[Math.floor(Math.random()*(arr.length-1))]}` : 'GAME OVER'; 
 	
 	let score = 0;
 	for(let i = 0; i < getPlayer().mobkilled.length; i++){
@@ -531,32 +532,39 @@ function dungeonRefresh(floor){
 }
 
 function stars(){
+	var count = 200;
 	var defaults = {
-		spread: 360,
-		ticks: 50,
-		gravity: 0,
-		decay: 0.94,
-		startVelocity: 30,
-		colors: ['FFE400', 'FFBD00', 'E89400', 'FFCA6C', 'FDFFB8']
-	  };
-	  
-	  function shoot() {
-		confetti({
-		  ...defaults,
-		  particleCount: 40,
-		  scalar: 1.2,
-		  shapes: ['star']
-		});
-	  
-		confetti({
-		  ...defaults,
-		  particleCount: 10,
-		  scalar: 0.75,
-		  shapes: ['circle']
-		});
-	  }
-	  
-	  setTimeout(shoot, 0);
-	  setTimeout(shoot, 100);
-	  setTimeout(shoot, 200);
+	  origin: { y: 0.7 }
+	};
+	
+	function fire(particleRatio, opts) {
+	  confetti({
+		...defaults,
+		...opts,
+		particleCount: Math.floor(count * particleRatio)
+	  });
+	}
+	
+	fire(0.25, {
+	  spread: 26,
+	  startVelocity: 55,
+	});
+	fire(0.2, {
+	  spread: 60,
+	});
+	fire(0.35, {
+	  spread: 100,
+	  decay: 0.91,
+	  scalar: 0.8
+	});
+	fire(0.1, {
+	  spread: 120,
+	  startVelocity: 25,
+	  decay: 0.92,
+	  scalar: 1.2
+	});
+	fire(0.1, {
+	  spread: 120,
+	  startVelocity: 45,
+	});
 }
