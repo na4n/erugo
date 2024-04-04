aefc0cca1d8af41de3b4f8f615b08ba625c7c8d1
Nathan Annareddy <nathan.annareddy@gmail.com>
2024-04-03 20:57:57 -0500

    fix buttons on mobile, move script to js file

--- js/main.js
+++ js/main.js
@@ -3,6 +3,25 @@ const VERSION = 3;
 const VALID_KEYS = ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown', 'e', 'a', 's', 'd', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 let gameOver = localStorage.getItem('gameOver') === null ? 0 : Number(localStorage.getItem('gameOver'));
+
+document.addEventListener('DOMContentLoaded', function() {
+	({ width: CHARWIDTH, height: CHARHEIGHT } = getCharacterDimensions('monospace', '@', '15.5px'));
+	document.getElementById('msg').style.height = `${getCharacterDimensions('times', 'H', '16').height}` + 'px';
+	enableDungeonEventListener();
+
+	dungeonRefresh();
+	updateStats();
+	document.getElementById('dungeon').click();
+	
+	const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+	if (isMobileDevice) {
+		const buttonDiv = document.getElementById('arrow');
+		buttonDiv.style.touchAction = 'none';
+		buttonDiv.innerHTML = '<button id="input" onclick="keyHandler(`ArrowUp`);">Up</button><br><button id="input" onclick="keyHandler(`ArrowLeft`);">Left</button><button id="input" onclick="keyHandler(`ArrowRight`);">Right</button><br><button id="input" onclick="keyHandler(`ArrowDown`);">Down</button><br><br>';
+		buttonDiv.innerHTML += '<button id="input" onclick="keyHandler(`e`)">E</button><a>&emsp;</a><button id="input" onclick="keyHandler(`s`)">S</button><button id="input" onclick="keyHandler(`d`);"">D</button><a>&emsp;</a><button id="input" onclick="keyHandler(`a`);">A</button><br><br>'
+	}
+});
+
 function divKeyDownHandler(event) {
 	if (VALID_KEYS.includes(event.key)) {
 		event.preventDefault();
