4d59fca1fb5fe41b601e6fec80babd015fbdb412
Nathan Annareddy <nathan.annareddy@gmail.com>
2024-04-23 16:47:31 -0500

    small fix, clipboard.js copying stats

--- js/clipboard.js
+++ js/clipboard.js
@@ -1,21 +1,26 @@
 addEventListener('copy', (event) => {
 	event.preventDefault();
 
-	const attackStat = gameOver === 0 ? PLAYER.strength : `${PLAYER.baseStats[0]}+${PLAYER.strength}`
-	const defenseStat = gameOver === 0 ? PLAYER.defense : `${PLAYER.baseStats[1]}+${PLAYER.defense}`
+	const attackStat = gameOver === 0 ? `?+${PLAYER.strength}` : `${PLAYER.baseStats[0]}+${PLAYER.strength}`
+	const defenseStat = gameOver === 0 ? `?+${PLAYER.defense}` : `${PLAYER.baseStats[1]}+${PLAYER.defense}`
 
-	const head = `Erugo @ na4n.github.io/erugo/\n\nLevel: ${PLAYER.level}          Gold: ${PLAYER.gold}\nStrength: ?+${attackStat}     Defense: ?+${defenseStat}`;
+	const head = `Erugo @ na4n.github.io/erugo/\n\nLevel: ${PLAYER.level}          Gold: ${PLAYER.gold}\nStrength: ${attackStat}     Defense: ${defenseStat}`;
 
 	if (gameOver == 0) {
-		event.clipboardData.setData("text/plain", `${head}\nHealth: ${(PLAYER.health).toFixed(2)}\n${boardTextRepresentation()}`);
+		event.clipboardData.setData("text/plain", `${head}\nHealth: ${(PLAYER.health).toFixed(2)}\n${boardEntityRepresentation()}`);
 	}
 	else {
 		event.clipboardData.setData("text/plain", `${head}\n\nGAME OVER\nKilled:\n%: ${PLAYER.mobKilled[0]}\n>: ${PLAYER.mobKilled[1]}\n~: ${PLAYER.mobKilled[2]}\n^: ${PLAYER.mobKilled[3]}\n&: ${PLAYER.mobKilled[4]}`);
 	}
 });
 
-function boardTextRepresentation() {
-	const dungeonText = dungeonBackground(FLOORDIMENSION).replaceAll("<br>", "\n").replaceAll("&nbsp;", " ");
+const dungeonBackgroundText = () => {
+	return dungeonBackground(FLOORDIMENSION).replaceAll("<br>", "\n").replaceAll("&nbsp;", " "); 
+}
+
+function boardEntityRepresentation() {
+	const dungeonText = dungeonBackgroundText();
+	
 	const rowArrayDungeonText = dungeonText.split("\n").map((row, i) => {
 		const rowArr = row.split("");
 		LOCATIONS.forEach(entity => {
@@ -27,4 +32,9 @@ function boardTextRepresentation() {
 	});
 
 	return rowArrayDungeonText.join("\n");
+}
+
+function boardGameOverRepresentation(){
+	const dungeonText = dungeonBackgroundText(); //dungeonBackground(FLOORDIMENSION).replaceAll("<br>", "\n").replaceAll("&nbsp;", " ");
+	return 1;
 }
